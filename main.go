bc56ef623b7ede9352e2a3eec2129871a1a846b3
Sergey Lazarenko <sergey.lazarenko.0241@mail.ru>
2025-07-11 02:37:44 +0300

    Добавление базы данных

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	tgClient "Telegram-bot/clients/telegram"
+	event_consumer "Telegram-bot/consumer/event-consumer"
+	"Telegram-bot/events/telegram"
+	"Telegram-bot/storage/sqlite"
+	"context"
+	"flag"
+	"log"
+)
+
+const (
+	tgBotHost         = "api.telegram.org"
+	sqliteStoragePath = "storage/sqlite/storage.db"
+	batchSize         = 100
+)
+
+func main() {
+
+	s, err := sqlite.New(sqliteStoragePath)
+	if err != nil {
+		log.Fatal("can't connect to storage: ", err)
+	}
+
+	if err := s.Init(context.TODO()); err != nil {
+		log.Fatal("can't init storage: ", err)
+	}
+
+	eventsProcessor := telegram.New(
+		tgClient.New(tgBotHost, mustToken()),
+		s,
+	)
+
+	log.Print("service started")
+
+	consumer := event_consumer.New(eventsProcessor, eventsProcessor, batchSize)
+
+	if err := consumer.Start(); err != nil {
+		log.Fatal("service is stopped", err)
+	}
+}
+
+func mustToken() string {
+	token := flag.String(
+		"tg-bot-token",
+		"",
+		"token for access to telegram bot",
+	)
+	flag.Parse()
+
+	if *token == "" {
+		log.Fatal("token is not specified")
+	}
+
+	return *token
+}
